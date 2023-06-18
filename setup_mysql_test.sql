898de7968a006c049f5c4ba11754e20c03c3247f
NewGit1999 <yasminebenali32@gmail.com>
2023-06-18 07:58:42 -0700

    script that prepares a MySQL server for the project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,9 +6,9 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- grant all priviledges on database
-GRANT ALL PRIVILEDGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEDGES
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES
 
 -- select priviledges on database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEDGES;
+FLUSH PRIVILEGES;
