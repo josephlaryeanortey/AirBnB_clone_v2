2a8da21f22ddd9cea631336cef62571a21132905
NewGit1999 <yasminebenali32@gmail.com>
2023-06-20 15:12:59 -0700

    update user class

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,6 +14,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='user',
+                              cascade='delete, all, delete-orphan')
     else:
         email = ''
         password = ''
