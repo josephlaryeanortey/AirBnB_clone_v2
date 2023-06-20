05f21fafd75c30a5d88e8c4791269bc44db0055f
NewGit1999 <yasminebenali32@gmail.com>
2023-06-20 16:43:12 -0700

    fixes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 
+from models import *
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
--- models/user.py
+++ models/user.py
@@ -12,10 +12,10 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship('Place', backref='user',
-                              cascade='delete, all, delete-orphan')
+                              cascade='all, delete,  delete-orphan')
         reviews = relationship('Review', backref='user',
                                cascade='all, delete, delete-orphan')
     else:
