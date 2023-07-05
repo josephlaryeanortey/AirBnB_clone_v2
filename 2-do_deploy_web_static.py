6efa86e35dd31205b8dad32eeff2330fbdba5f01
samuelselasi <samelsekasi@gmail.com>
2023-07-05 22:34:14 +0000

    Add fab file that deploys archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
 """Python module to deploy archive on web servers"""
-from fabric.api import env, put, run, sudo
+from fabric.api import env, put, run
 from os import path
 
 env.hosts = ['3.90.70.66', '100.26.231.45']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """Deploy archive to web server"""
-    if path.isfile(archive_path) is False:
-        return False
+    """Function to distribute an archive to web servers"""
     try:
-        f_name = archive_path.split("/")[-1]
-        no_ext = f_name.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
+        if not (path.exists(archive_path)):
+            return False
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(f_name, path_no_ext))
-        run("rm /tmp/{}".format(f_name))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
+        name = archive_path[-18:-4]
 
-        return True
-    except:
+        put(archive_path, '/tmp/')
+        run("sudo mkdir -p /data/web_static/releases/web_static_{}/\
+                ".format(name))
+        run("sudo tar -xzf /tmp/web_static_{}.tgz -C \
+                /data/web_static/releases/web_static_{}/".format(name, name))
+        run("sudo rm /tmp/web_static_{}.tgz".format(name))
+        run("sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+                /data/web_static/releases/web_static_{}/".format(name, name))
+        run("sudo rm -rf /data/web_static/releases/\
+                web_static_{}/web_static".format(name))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/\
+                web_static_{}/ /data/web_static/current".format(name))
+    except Exception:
         return False
+    return True
