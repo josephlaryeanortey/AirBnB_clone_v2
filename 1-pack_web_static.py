65e1495be070c1a481e76880d1b4f0ae2b0d8260
samuelselasi <samelsekasi@gmail.com>
2023-07-05 21:23:23 +0000

    Fix fab file that compresses web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Python module to compress all web static files"""
+from datetime import datetime
 from fabric.api import local
-from time import strftime
+from os.path import isdir
 
 
 def do_pack():
     """A function that generates .tgz archive from contents of web_static"""
-
-    timenow = strftime("%Y%M%d%H%M%S")
     try:
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(timenow)
-        local("tar -cvzf {} web_static/".format(filename))
-        return filename
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except:
         return None
