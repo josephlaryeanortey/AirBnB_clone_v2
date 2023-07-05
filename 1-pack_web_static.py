8cafe3b0424ccbbbec4452cfcfbac98edd8d120d
samuelselasi <samelsekasi@gmail.com>
2023-07-05 20:32:02 +0000

    Fix fab file that compresses web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,11 @@ from time import strftime
 def do_pack():
     """A function that generates .tgz archive from contents of web_static"""
 
-    name = strftime('%Y%m%d%H%M%S')
+    timenow = strftime("%Y%M%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(name))
-        return ("versions/web_static_{}.tgz".format(name))
-    except Exception:
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
         return None
