7b9e77df28d5f207d99a6b08bc32924ee2469dca
samuelselasi <samelsekasi@gmail.com>
2023-07-06 10:39:43 +0000

    Test task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 """Python module to compress all web static files"""
+from fabric.context_managers import settings
 from fabric.api import local
 from time import strftime
+import os
 
 
 def do_pack():
     """A function that generates .tgz archive from contents of web_static"""
 
     name = strftime('%Y%m%d%H%M%S')
+    archive_path = "versions/web_static_{}.tgz".format(name)
+    print("Packing web_static to {}".format(archive_path))
+
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(name))
+        os.makedirs("versions")
+    except OSError:
+        return None
+
+    result = local("tar -czvf versions/web_static_{}.tgz web_static".format(name))
+    archive_size = os.path.getsize(archive_path)
+    print("web_static packed: {} -> {}Bytes".format(archive_path,
+          archive_size))
+
+    if result.stdout.strip() == '0':
         return ("versions/web_static_{}.tgz".format(name))
-    except Exception:
+    else:
         return None
