c4e0bb14ebb5da84ccef0b1fe2ae80c29ea02f09
samuelselasi <samelsekasi@gmail.com>
2023-07-06 14:20:50 +0000

    Fix task3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,53 +1,66 @@
 #!/usr/bin/python3
 """A module that deploys archives on web servers"""
 from fabric.api import local, env, put, run
-from time import strftime
-from os import path
+from datetime import datetime
+import os
 
 env.hosts = ['3.90.70.66', '100.26.231.45']
 
 
 def do_pack():
     """A function that generates .tgz archive from contents of web_static"""
-    name = strftime('%Y%m%d%H%M%S')
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(name))
-        return ("versions/web_static_{}.tgz".format(name))
-    except Exception:
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
 
 
 def do_deploy(archive_path):
     """Function to distribute an archive to web servers"""
-    try:
-        if not (path.exists(archive_path)):
-            return False
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        name = archive_path[-18:-4]
-
-        put(archive_path, '/tmp/')
-        run("sudo mkdir -p /data/web_static/releases/web_static_{}/\
-                ".format(name))
-        run("sudo tar -xzf /tmp/web_static_{}.tgz -C \
-                /data/web_static/releases/web_static_{}/".format(name, name))
-        run("sudo rm /tmp/web_static_{}.tgz".format(name))
-        run("sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-                /data/web_static/releases/web_static_{}/".format(name, name))
-        run("sudo rm -rf /data/web_static/releases/\
-                web_static_{}/web_static".format(name))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/\
-                web_static_{}/ /data/web_static/current".format(name))
-    except Exception:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
 
 
 def deploy():
     """Function that deploys archives"""
-    archive_path = do_pack()
-    if archive_path is None:
+    file = do_pack()
+    if file is None:
         return False
-    success = do_deploy(archive_path)
-    return success
+    return do_deploy(file)
