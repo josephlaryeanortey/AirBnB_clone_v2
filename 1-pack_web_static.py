95fe2d373e51cb9935442e365ac1c0e47102a64f
samuelselasi <samelsekasi@gmail.com>
2023-07-06 10:54:33 +0000

    Test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     name = strftime('%Y%m%d%H%M%S')
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(name))
+        local("tar -czvf versions/web_static_{}.tgz web_static".format(name))
         return ("versions/web_static_{}.tgz".format(name))
     except Exception:
         return None
