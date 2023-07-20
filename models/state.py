3e734fba477b888714238b2a7ca628b39264773f
samuelselasi <samelsekasi@gmail.com>
2023-07-20 21:28:31 +0000

    pycache stuff

--- models/state.py
+++ models/state.py
@@ -17,6 +17,11 @@ class State(BaseModel, Base):
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """relationship between city and state"""
